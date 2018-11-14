0fe33a0c3a1bf449ecfb0fa56315e420fae2f49b
William-9 <972471927@qq.com>
2018-11-14 23:04:37 +0800

    test类

---
+++ src/chapter5/section5/Test.java
@@ -0,0 +1,14 @@
+package chapter5.section5;
+
+public class Test {
+    private static int i;
+    public static void main(String[] args) {
+        int j = 0;
+        System.out.println(i+" "+j);
+
+        char a = '3';//如何最快得到  int类型的3
+        char b = '0';
+        int c = a - b;
+        System.out.println(c);
+    }
+}
