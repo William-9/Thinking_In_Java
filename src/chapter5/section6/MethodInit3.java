396dbc0cd19be150cc9ed91b3a1db3ae39bb6ed0
William-9 <972471927@qq.com>
2018-11-14 23:05:15 +0800

    5.6 成员初始化

---
+++ src/chapter5/section6/MethodInit3.java
@@ -0,0 +1,8 @@
+package chapter5.section6;
+
+public class MethodInit3 {
+    //! int j = g(i); // Illegal forward reference
+    int i = f();
+    int f() { return 11; }
+    int g(int n) { return n * 10; }
+}
