3b2eaaad4b24ee07dbfccdd2946c279e1215ca07
William-9 <972471927@qq.com>
2018-11-21 21:59:59 +0800

    6.2 Java访问权限修饰词

---
+++ src/chapter6/section2/Cookie.java
@@ -0,0 +1,10 @@
+package chapter6.section2;
+
+public class Cookie {
+    public Cookie() {
+        System.out.println("Cookie constructor");
+    }
+    protected void bite() {
+        System.out.println("bite");
+    }
+}
